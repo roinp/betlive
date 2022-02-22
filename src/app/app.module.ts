22d69a8f0e044494259dfe481269077edc62ed75
Roin Putkaradze <putkaradzeroin1@gmail.comm>
2022-02-22 18:01:15 +0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainAddsComponent } from './main-adds/main-adds.component';
+// import { PlayButton } from './playng-section/playng-section.component.spec';
+import { CashbackSectionComponent } from './cashback-section/cashback-section.component';
+import { PlayButtonComponent } from './playng-section/play-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainAddsComponent,
+    PlayButtonComponent,
+    CashbackSectionComponent
   ],
   imports: [
     BrowserModule
