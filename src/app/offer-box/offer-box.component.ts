8a3e1443eb6a853743c2744ee422cf481749c760
Roin Putkaradze <putkaradzeroin1@gmail.comm>
2022-02-23 23:49:15 +0400

    offer-box component

---
+++ src/app/offer-box/offer-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-offer-box',
+  templateUrl: './offer-box.component.html',
+  styleUrls: ['./offer-box.component.css']
+})
+export class OfferBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
